57e894c79a6f15c383c357f7dad489d5259be4c0
Scrien <109849850+ScRien@users.noreply.github.com>
2023-01-22 11:16:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+def Factorial_Calculation():
+    try:
+        sayi = int(input("Sayı giriniz: "))
+    except ValueError:
+        print("Lütfen Sayı giriniz!")
+        SayiAl()
+        
+    sonuc = 1
+
+    while sayi > 0:
+        sonuc = sonuc * sayi
+        sayi -= 1
+        print("Sonuç: {}".format(sonuc))
+        
+Factorial_Calculation()
+
